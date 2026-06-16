323d816ca4db11918e86b708e53bfd0f9248a217
Jacob Mossberg <jacob@salusmea.com>
2026-06-16 21:03:15 +0200

    It works!

--- divergent_change/src/CustomerService.ts
+++ divergent_change/src/CustomerService.ts
@@ -2,6 +2,7 @@ export class CustomerService {
     private emailValidator = new EmailValidator();
     private customerDisplayFormatter = new CustomerDisplayFormatter();
     private loyaltyService = new LoyaltyService();
+    private accountStatusService = new AccountStatusService();
 
     public isValidEmail(email: string | null): boolean {
         return this.emailValidator.isValidEmail(email);
@@ -16,12 +17,7 @@ export class CustomerService {
     }
 
     public determineAccountStatus(daysSinceLastLogin: number): string {
-        if (daysSinceLastLogin > 365) {
-            return "INACTIVE";
-        } else if (daysSinceLastLogin > 30) {
-            return "DORMANT";
-        }
-        return "ACTIVE";
+        return this.accountStatusService.determineAccountStatus(daysSinceLastLogin);
     }
 }
 
@@ -45,5 +41,16 @@ class CustomerDisplayFormatter {
 class LoyaltyService {
     public calculateLoyaltyPoints(numberOfPurchases: number): number {
         return numberOfPurchases * 10;
+    }
+}
+
+class AccountStatusService {
+    public determineAccountStatus(daysSinceLastLogin: number): string {
+        if (daysSinceLastLogin > 365) {
+            return "INACTIVE";
+        } else if (daysSinceLastLogin > 30) {
+            return "DORMANT";
+        }
+        return "ACTIVE";
     }
 }
