35a2e341a0b474fa4c72139a9487b50390ac81c1
Jacob Mossberg <jacob@salusmea.com>
2026-06-16 20:47:22 +0200

    It works!

--- divergent_change/src/CustomerService.ts
+++ divergent_change/src/CustomerService.ts
@@ -1,12 +1,13 @@
 export class CustomerService {
     private emailValidator: EmailValidator = new EmailValidator();
+    private customerDisplayFormatter: CustomerDisplayFormatter = new CustomerDisplayFormatter();
 
     public isValidEmail(email: string | null): boolean {
         return this.emailValidator.isValidEmail(email);
     }
 
     public formatDisplayName(firstName: string, lastName: string): string {
-        return firstName.trim() + " " + lastName.trim().toUpperCase();
+        return this.customerDisplayFormatter.formatDisplayName(firstName, lastName);
     }
 
     public calculateLoyaltyPoints(numberOfPurchases: number): number {
@@ -31,5 +32,11 @@ class EmailValidator {
 
         const emailRegex = /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/;
         return emailRegex.test(email);
+    }
+}
+
+class CustomerDisplayFormatter {
+    public formatDisplayName(firstName: string, lastName: string): string {
+        return firstName.trim() + " " + lastName.trim().toUpperCase();
     }
 }
