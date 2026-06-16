6bbc6111f7b97cc8c709d849a8415dda4ea132b8
Jacob Mossberg <jacob@salusmea.com>
2026-06-16 20:21:02 +0200

    It works!

--- comments/src/X1.ts
+++ comments/src/X1.ts
@@ -2,9 +2,7 @@ export class X1 {
     static m(lowerBound: number, upperBound: number): number {
         let p = 0;
 
-        // Iterate from lower bound (q) to upper bound (z)
         for (let i = lowerBound; i <= upperBound; i++) {
-            // Add square of each number in the range
             p += X1.square(i);
         }
 
